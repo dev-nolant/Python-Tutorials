d85761da7544d6c581ed18aa4d8c86cd2e24fe81
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-01-18 23:29:16 -0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 import time # !! important !! -- Shows the standard call for importing a module. In this case I imported 'time', which automatically comes with Python when Python is installed.
+
 print("Hi") # !! important !! -- Shows the standard function for printing text out for the program user to see in console! -- In this case, the person who runs the code will be met with 'Hi' on their screen.
+
 time.sleep(10) # This shows a simple use of the module we imported earlier, 'time'. In this case, we have 'time' making our program wait 10 seconds before moving on to the functions below, which in this case is just 'print("Hi")'.
+
 print("Hi 2") # After the time is over, the user will be met with "Hi", then the window will close immediately after! The window closes because the program is done running
 # ^^^ We could make this 'input("Hi 2")' if we wanted the window to not instantly close after completion, this will make the user have to click the 'x' or press enter to close the program, because the function 'input' is waiting for an input followed with enter. 
 
