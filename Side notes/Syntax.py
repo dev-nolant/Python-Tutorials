4eb5cd6175f249265001a9149d77ebad3ac72956
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-01-18 23:27:25 -0700

    Add files via upload

---
+++ Side notes/Syntax.py
@@ -0,0 +1,7 @@
+import time # A simple import, displayed and reasoned in '1.py'
+try: # This you will learn, but this is a try, except exception catch method. This will try whatever code is right after the 'try' expression and if it fails, it will move to the 'except' section.
+    print("hi") # !! SYNTAX !! -- Here is where we see our first example of an altered syntax. This being 'tabbed'(basically just pressing tab to indent it) over puts it inside of the 'try' parent. This is the code that is being run inside of 'try', which if it failed or through an error, would then run the code under the 'except' parent.
+except: # As 'else' is to 'if', this catches the error thrown by anything that failed in the 'try' parent, and runs whatever code is under this parent. 'except'
+    print("your mom") # !! SYNTAX !! -- This is the code set to run if the 'except' parent is called. 'except' parent is only called/put into play if whatever code under the 'try' parent fails
+
+# Hopefully this wasn't too hard to understand. Syntaxing can be very difficult, but it's also very important, with enough practice you can be able to spot a syntaxing error in no time!
