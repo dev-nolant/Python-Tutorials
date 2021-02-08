9d411ed0a54e07b9d8c3613253c8968aa3af1e95
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-02-08 12:30:53 -0700

    EASY MODE ADDED

---
+++ Tutorial by number/3_5_easy.py
@@ -0,0 +1,5 @@
+#This is an example of what you will face after an error. Fix this code! Nothing that we haven't talked about will have to be fixed. This is level Easy.
+#EASY
+input1 = input("Enter a simple number: ")
+foo = int(1) * str(input1)
+print(foo)
