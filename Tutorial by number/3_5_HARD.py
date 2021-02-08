cc4446d144602d8e6c9f914af117b7b0d3346bcd
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-02-08 12:34:30 -0700

    Create 3_5_HARD.py

---
+++ Tutorial by number/3_5_HARD.py
@@ -0,0 +1,7 @@
+#I do not expect you to be able to figure this out just yet, but I want to give you a challenge! There are more than one errors in this problem
+import "os.py"
+os.system(echo Hi)
+input("Enter to continue")
+print(12 * 2)
+print("That is the answer to 12 * 2!"(
+#Easy to experienced, hard to new learners, so do not feel discouraged.
