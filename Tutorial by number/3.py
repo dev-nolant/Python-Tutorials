d3cde6bd633557cc53d9a44b5e10663229e3fa34
Nolan Taft <54606283+dev-nolant@users.noreply.github.com>
2021-02-08 11:06:48 -0700

    Update 3.py

--- Tutorial by number/3.py
+++ Tutorial by number/3.py
@@ -1,1 +1,8 @@
-
+# We'll now take a look at basic input handling! This program we'll make the user have to input two numbers and we will multiply them!
+num1 = input("Enter your first number: ")#We've already learned this, but now we're setting different variables. Like in this case we're using 'num1'
+#^ instead of foo like the last one
+num2 = input("Enter your second number: ")
+#We can have a variable solve the question, then print the variable like so:
+answer = num1 * num2 # '*' is the equivalent to the multiplication symbol in math!
+#answer will now be what num1 times num2 is!
+print(answer) #and now we print it! This will print the answer to num1 * num2!!
